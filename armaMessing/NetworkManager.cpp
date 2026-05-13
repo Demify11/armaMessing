2fcd7b2172180289acf6104f925be905e9b7cdaf
Demetri-DLL <Demetrious3000@gmail.com>
2026-05-13 20:47:46 +0100

    Working Entity Manager, with efficient buffer reading using a struct.

--- armaMessing/NetworkManager.cpp
+++ armaMessing/NetworkManager.cpp
@@ -10,31 +10,63 @@ void NetworkManager::Cache(bool State) {
 void NetworkManager::CacheNetworkClient(bool State) {
 
 	if (State) {
-		m_Identities = Coms->ReadVirtual<UINT64>(m_Base + Offsets::NetworkClient); //290
+		m_NetworkClient = Coms->ReadVirtual<UINT64>(m_Base + Offsets::NetworkClient); //290
 	}
 
 }
 
-void NetworkManager::CacheIdentities(bool State) {
+struct IdentityEntry
+{
+	uint8_t  pad0[0x8];
+	int32_t  m_NetworkId;
+
+	uint8_t  pad1[0x150 - 0xC];
+	uint64_t m_NamePtr;
+
+	uint8_t  pad2[0x290 - 0x158];
+};
+
+static_assert(offsetof(IdentityEntry, m_NetworkId) == 0x8);
+static_assert(offsetof(IdentityEntry, m_NamePtr) == 0x150);
+static_assert(sizeof(IdentityEntry) == 0x290);
+
+void NetworkManager::CacheIdentities(bool State) { //we cant really use the autoarray class becuase the class assumes data is located
+	// at the addresses of each index, however this list is in stride/inline. need to update autoarray for this senario
 
 	if (State) {
 
-		auto Array = AutoArray(m_Identities + Offsets::PlayerIdentities);
+		auto Entry = Coms->ReadVirtual<UINT64>(m_NetworkClient + Offsets::PlayerIdentities);
+		auto ListSize = Coms->ReadVirtual<int>(m_NetworkClient + Offsets::PlayerIdentities + 0x8);
 
-		auto ArraySize = Array.GetSize();
+		//uint8_t* Buffer = new uint8_t[ListSize * 0x290];
 
-		if (!Array.Allocate(ArraySize, 0x0, 0x290));
+		IdentityEntry* Entries = new IdentityEntry[ListSize];
+
+		//memset(Entries, 0x00, ListSize * 0x290);
+
+		if (!Coms->ReadVirtualBuffer(Entry, Entries, ListSize * 0x290)) {
+			delete[] Entries;
 			return;
+		}
 
-		for (int i = 0; i < ArraySize; i++) {
+		//auto* entries = reinterpret_cast<IdentityEntry*>(Buffer); dont need this anymore becuase we directly make a buffer of entries
 
+		for (int i = 0; i < ListSize; i++) {
+			
+			IdentityEntry& entry = Entries[i];
 			Identity Temp;
-			Temp.m_Base = Array.Get(i);
-			Temp.Cache(State);
+			Temp.m_NetworkId = entry.m_NetworkId;
+
+			if (entry.m_NamePtr)
+			{	
+				Temp.m_Name = Coms->ReadString(entry.m_NamePtr + 0x10);
+			}
 
 			Identities[Temp.m_NetworkId] = Temp;
 
 		}
+		
+		delete[] Entries;
 
 	}
 
